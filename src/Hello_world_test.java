ba62880a378986357d70259df7da412da8faa5f4
fizik1580 <48645575+fizik1580@users.noreply.github.com>
2019-04-13 19:48:10 +0300

    Initial commit

---
+++ src/Hello_world_test.java
@@ -0,0 +1,8 @@
+/** This is test-comment
+ * create: Demin
+ */
+public class Hello_world_test {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
